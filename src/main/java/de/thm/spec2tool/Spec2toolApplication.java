027670899aa67c96afd2f0db303263570ca4764a
Josia Menger <josia.menger@zdh.thm.de>
2025-09-17 12:38:47 +0200

    chore: initial commit

---
+++ src/main/java/de/thm/spec2tool/Spec2toolApplication.java
@@ -0,0 +1,13 @@
+package de.thm.spec2tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spec2toolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spec2toolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/thm/spec2tool/Spec2toolApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thm.spec2tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spec2toolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
